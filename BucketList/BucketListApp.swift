5f9aad8cc053ca98f51641541e5abeb435413feb
suryasai <suryasaigopalam@gmail.com>
2024-05-24 11:40:27 +0530

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by surya sai on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
